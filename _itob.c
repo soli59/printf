d77968213c0766882bfa71cfe85d68da30df3ee4
mohamed <gega7816@gmail.com>
2023-10-18 08:44:14 -0700

    updated

--- _itob.c
+++ _itob.c
@@ -1,48 +1,8 @@
 #include "main.h"
 
 /**
- * _itoa_recursive - Helper function to convert integer to a specified base
- * using recursion.
- *
- * @number: the number to convert
- * @buffer: the string buffer to store the result
- * @base: the base to convert @number to
- * @len: the current length of the result string
- */
-void _itoa_recursive(ssize_t number, char *buffer, int base, size_t *len)
-{
-	if (number == 0)
-	{
-		buffer[(*len)++] = '0';
-		return;
-	}
-
-	ssize_t remainder = number % base;
-
-	if (remainder < 0)
-	{
-		remainder = -remainder; /*Get the absolute value of the remainder*/
-
-	}
-
-	if (remainder > 9)
-	{
-		buffer[(*len)++] = 'a' + (remainder - 10);
-	}
-	else
-	{
-		buffer[(*len)++] = '0' + remainder;
-	}
-
-	if (number / base != 0)
-	{
-		_itoa_recursive(number / base, buffer, base, len);
-	}
-}
-
-/**
  * _itob - convert integer to the specified base and stores the result
- * as a string.
+ * as a string
  *
  * @number: the number to convert
  * @buffer: the string buffer to store the result
@@ -51,18 +11,30 @@ void _itoa_recursive(ssize_t number, char *buffer, int base, size_t *len)
 void _itob(ssize_t number, char *buffer, int base)
 {
 	size_t len = 0;
+	ssize_t sign = number;
 
 	if (number < 0)
-	{
-		buffer[len++] = '-';
-		number = -number; /* Get the absolute value of the number*/
-	}
+		number = -number; /* get the absolute value of n */
 
-	if (base < 2 || base > 16)
+	switch (base) /* Check base number */
 	{
-		return;
-	}
+		/* Accepted bases */
+		case BIN:
+		case OCT:
+		case DEC:
+		case HEX:
+			do {
+				buffer[len++] = (number % base > 9) ? ('a' + (number  % base - 10)) :
+					(number % base + '0');
+			} while ((number /= base) > 0);
 
-	_itoa_recursive(number, buffer, base, &len);
-	buffer[len] = '\0';
+			if (sign < 0)
+				buffer[len++] = '-';
+
+			buffer[len] = '\0';
+			_reverse_str(buffer, len); /* Get the correct representation */
+			break;
+		default:
+			return;
+	}
 }
